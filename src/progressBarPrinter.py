fb940f32a64604872aac4e29e87aea3844052d36
NicolasBellanich <nicolasbellanich@gmail.com>
2019-12-23 18:41:30 -0300

    Comiit 1 de nico

--- src/progressBarPrinter.py
+++ src/progressBarPrinter.py
@@ -7,4 +7,4 @@ def print_progress_bar(value, end_value, message ="", start_value=0, bar_length=
     spaces = ' ' * (bar_length - len(arrow))
 
     sys.stdout.write("\r {}[{}] {:.2f}%".format(message,arrow + spaces, float(round(percent * 100, 2))))
-    sys.print('HOLA NICO')
+    sys.print('HOLA Santi')
